f6eb38078172a2d05db2d2695b4f6a3b723cd7c2
Renan Ciciliato <renanciciliato@gmail.com>
2020-02-04 09:51:05 -0200

    Buscando produtos da API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './styles.css';
 import Header from './components/Header';
+import Main from './pages/main';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Main />
       </div>
     );
   }
