65d283ffb4e3fffd7e5f7c7d557bca3ab8833b6e
Renan Ciciliato <renanciciliato@gmail.com>
2020-02-04 09:32:30 -0200

    Criando header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import './styles.css';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello Rocketseat</h1>
+        <Header />
       </div>
     );
   }
